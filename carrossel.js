e4db76f6146357887036efc08cacb7f0e4c5c317
babi2707 <90732683+babi2707@users.noreply.github.com>
2021-11-24 16:59:41 -0300

    tentativa (erro)

--- carrossel.js
+++ carrossel.js
@@ -1,60 +1,56 @@
 onload = () => {
-    //---------- dados fakes ----------
-    var imagens = [
-      {
-        id: 0,
-        nomeInst: "Instituição de Caridade",
-        categoria: 0,
-        imagem: "logo2.png",
-      },
-  
-      {
-        id: 1,
-        nomeInst: "Instituição de Roupas",
-        categoria: 1,
-        imagem: "logo4.png",
-      },
-  
-      {
-        id: 2,
-        nomeInst: "Instituição de Livros",
-        categoria: 0,
-        imagem: "logo3.png",
-      },
-  
-      {
-        id: 3,
-        nomeInst: "Instituição de Brinquedos",
-        categoria: 3,
-        imagem: "logo4.png",
-      },
-    ];
-  
-    localStorage.setItem("fakes", JSON.stringify(imagens));
-
-    let carrossel = document.getElementById('img_carrossel');
-
-    let imgs = '';
-
-    let fakes = imagens;
-
-    for (let i = 0; i < fakes.lenght; i++)
+  //---------- dados fakes ----------
+  var imagens = [
     {
-        let src = fakes[i];
+      id: 0,
+      nomeInst: "Instituição de Caridade",
+      categoria: 0,
+      imagem: "imgs/logoo.png",
+    },
 
-        if (i == 0) 
-        {
-            imgs += `<div class="carousel-item active">`;
-        }
-        else 
-        {
-            imgs += `<div class="carousel-item">`;
-        }
+    {
+      id: 1,
+      nomeInst: "Instituição de Roupas",
+      categoria: 1,
+      imagem: "imgs/logo4.png",
+    },
 
-        imgs += `<img src="${src.imagem}" alt="Imagem ${i}" >`;
-    };
+    {
+      id: 2,
+      nomeInst: "Instituição de Livros",
+      categoria: 0,
+      imagem: "imgs/logo3.png",
+    },
 
-    carrossel.innerHTML = imgs;
+    {
+      id: 3,
+      nomeInst: "Instituição de Brinquedos",
+      categoria: 3,
+      imagem: "imgs/logo4.png",
+    },
+  ];
 
-  };
-  
+  //------- guardando os dados fakes no localStorage -------
+  localStorage.setItem("fakes", JSON.stringify(imagens));
+
+  //------- apresentar as imagens do localStorage -------
+  var divImagens = document.getElementById("carrossel_img");
+  var imgs = JSON.parse(localStorage.getItem("fakes"));
+
+  let colocar = "";
+
+  for (let i = 0; i < imgs.lenght; i++) {
+    let imgInfo = imgs[i];
+
+    if (i == 0) {
+      colocar += `<div class="carousel-item active">`;
+    } else {
+      colocar += `<div class="carousel-item">`;
+    }
+
+    colocar += `<img class="img_car" src="${imgInfo.imagem}" alt="ID ${imgInfo.id}" width="100" height="100">
+      </div>`;
+  }
+
+  divImagens.innerHTML = colocar;
+};
